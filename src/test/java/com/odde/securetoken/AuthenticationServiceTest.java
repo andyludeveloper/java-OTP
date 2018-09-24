8ec896aad39cf9b8ebe8dcdf4e101ec3eb1298cd
Joey Chen <jay.hatelove@gmail.com>
2018-09-24 23:55:16 +0800

    refactor mock test cases and add test: should not log when valid

--- src/test/java/com/odde/securetoken/AuthenticationServiceTest.java
+++ src/test/java/com/odde/securetoken/AuthenticationServiceTest.java
@@ -6,9 +6,7 @@ import org.mockito.ArgumentCaptor;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 public class AuthenticationServiceTest {
 
@@ -28,16 +26,39 @@ public class AuthenticationServiceTest {
 
     @Test
     public void should_log_account_when_invalid() {
+        whenInvalid();
+        shouldLogWithKeyWords("joey", "login failed");
+    }
+
+    @Test
+    public void should_not_log_account_when_valid() {
+        whenValid();
+        shouldNotLogAnything();
+    }
+
+    private void shouldNotLogAnything() {
+        verify(mockLogger, never()).save(anyString());
+    }
+
+    private void whenValid() {
         givenProfile("joey", "91");
         givenToken("000000");
+        target.isValid("joey", "91000000");
+    }
 
+
+    private void whenInvalid() {
+        givenProfile("joey", "91");
+        givenToken("000000");
         target.isValid("joey", "wrong password");
+    }
 
+    private void shouldLogWithKeyWords(String account, String action) {
         ArgumentCaptor<String> captor = ArgumentCaptor.forClass(String.class);
         verify(mockLogger).save(captor.capture());
 
         String message = captor.getValue();
-        assertThat(message).contains("joey", "login failed");
+        assertThat(message).contains(account, action);
     }
 
     private void shouldBeValid(String account, String password) {
