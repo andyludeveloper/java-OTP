77c216972883a61160b6098ea8a741ab3f048436
Joey Chen <jay.hatelove@gmail.com>
2018-09-24 22:53:29 +0800

    create stub objects by mockito

--- src/test/java/com/odde/securetoken/AuthenticationServiceTest.java
+++ src/test/java/com/odde/securetoken/AuthenticationServiceTest.java
@@ -3,13 +3,22 @@ package com.odde.securetoken;
 import org.junit.Test;
 
 import static org.junit.Assert.assertTrue;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class AuthenticationServiceTest {
 
     @Test
     public void is_valid_test() {
-//        AuthenticationService target = new AuthenticationService();
-        AuthenticationService target = new AuthenticationService(new StubProfile(), new StubMyToken());
+        Profile stubProfile = mock(Profile.class);
+        when(stubProfile.getPassword("joey")).thenReturn("91");
+
+        MyToken stubToken = mock(MyToken.class);
+        when(stubToken.getRandom(anyString())).thenReturn("000000");
+
+        AuthenticationService target = new AuthenticationService(stubProfile, stubToken);
+//        AuthenticationService target = new AuthenticationService(new StubProfile(), new StubMyToken());
 
         boolean actual = target.isValid("joey", "91000000");
 
