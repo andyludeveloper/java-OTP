ff6a94574d7244b7e54bea91b683e2086a5a215f
AndyLu <andyludeveloper@gmail.com>
2020-06-06 12:51:56 +0800

    Add Unit test. Method 1. Inversion of control Method 2. Override

--- src/test/java/com/odde/securetoken/AuthenticationServiceTest.java
+++ src/test/java/com/odde/securetoken/AuthenticationServiceTest.java
@@ -8,11 +8,56 @@ public class AuthenticationServiceTest {
 
     @Test
     public void is_valid_test() {
-        AuthenticationService target = new AuthenticationService();
+        AuthenticationService target = new AuthenticationService(new FakeProfile(), new FakeToken());
 
-        boolean actual = target.isValid("joey", "91000000");
+        boolean actual = target.isValid("andy", "00001111");
 
         assertTrue(actual);
     }
+    @Test
+    public void is_valid_test2() {
+        AuthenticationService target = new AuthenticationService(new FakeProfile2(), new FakeToken2());
 
+        boolean actual = target.isValid("andy", "00001111");
+
+        assertTrue(actual);
+    }
 }
+
+ class FakeProfile implements IProfile{
+
+    @Override
+    public String getPassword(String account) {
+        if(account.equals("andy"))
+            return "0000";
+        else
+            return "2222";
+    }
+}
+
+ class FakeToken implements  IToken{
+
+    @Override
+    public String getRandom(String account) {
+        return "1111";
+    }
+}
+class FakeProfile2 extends ProfileDao{
+
+    @Override
+    public String getPassword(String account) {
+        if(account.equals("andy"))
+            return "0000";
+        else
+            return "2222";
+    }
+}
+
+ class FakeToken2 extends RsaTokenDao{
+
+    @Override
+    public String getRandom(String account) {
+        return "1111";
+    }
+}
+
