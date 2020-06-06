524146d7855ae1d414a7eefe9a260d67ee88bb1d
AndyLu <andyludeveloper@gmail.com>
2020-06-06 13:30:44 +0800

    Using mokito

--- src/test/java/com/odde/securetoken/AuthenticationServiceTest.java
+++ src/test/java/com/odde/securetoken/AuthenticationServiceTest.java
@@ -3,6 +3,9 @@ package com.odde.securetoken;
 import org.junit.Test;
 
 import static org.junit.Assert.assertTrue;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class AuthenticationServiceTest {
 
@@ -17,6 +20,34 @@ public class AuthenticationServiceTest {
     @Test
     public void is_valid_test2() {
         AuthenticationService target = new AuthenticationService(new FakeProfile2(), new FakeToken2());
+
+        boolean actual = target.isValid("andy", "00001111");
+
+        assertTrue(actual);
+    }
+    @Test
+    public void is_valid_test3() {
+        IProfile profile = mock(IProfile.class);
+        when(profile.getPassword("andy")).thenReturn("0000");
+
+        IToken token = mock(IToken.class);
+        when(token.getRandom(anyString())).thenReturn("1111");
+
+        AuthenticationService target = new AuthenticationService(profile, token);
+
+        boolean actual = target.isValid("andy", "00001111");
+
+        assertTrue(actual);
+    }
+    @Test
+    public void is_valid_test4() {
+        ProfileDao profile = mock(ProfileDao.class);
+        when(profile.getPassword("andy")).thenReturn("0000");
+
+        RsaTokenDao token = mock(RsaTokenDao.class);
+        when(token.getRandom(anyString())).thenReturn("1111");
+
+        AuthenticationService target = new AuthenticationService(profile, token);
 
         boolean actual = target.isValid("andy", "00001111");
 
